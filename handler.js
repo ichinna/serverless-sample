351ed35beb2e929f0357faede49e8ded7c4b44ea
chinna <chinnapave@gmail.com>
2021-02-10 14:25:51 -0600

    chore: DD integration

--- handler.js
+++ handler.js
@@ -3,6 +3,14 @@
 const { datadog, sendDistributionMetric } = require("datadog-lambda-js");
 
 const exec = async event => {
+  sendDistributionMetric(
+    "formulaExecution", // Metric name
+    1,                      // Metric value
+    "stage:dev",            // Associated tag
+    "region:us-east-1",             // Another tag
+    "service:sample"
+  );
+
   return {
     statusCode: 200,
     body: JSON.stringify(
